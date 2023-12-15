7c6e8d8dcd189d784d9004eaff9e3f759ae48ecf
César Henrique Resende Soares <73477344+CesarHRS@users.noreply.github.com>
2023-12-15 14:56:42 -0300

    Update main.ino

--- Source/main.ino
+++ Source/main.ino
@@ -1,294 +1,210 @@
-#include "SD.h"
-#include"SPI.h"
-#include <Wire.h> 
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+int sensorValue = 0;        // value read from the pot
+int outputValue = 0;        // value output to the PWM (analog out)
+int sensorValue2 = 0;        // value read from the pot
+int outputValue2 = 0;   
 
-//configuração do display
-#define SCREEN_WIDTH 128 
-#define SCREEN_HEIGHT 64 
-#define OLED_RESET     4
-#define SCREEN_ADDRESS 0x3C 
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-#include "RTCManager.h"
-RTCManager rtcManager;
-//configuração do modulo sd 
-#define CSpin  10
-String dataString =""; // holds the data to be written to the SD card
-float sensorReading1 = 0.00; // value read from your first sensor
-float sensorReading2 = 0.00; // value read from your second sensor
-float sensorReading3 = 0.00; // value read from your third sensor
-File sensorData;
-
-//configuração do sensor ntc
-#define NTCPin  A2
-#define SERIESRESISTOR 10000
-#define NOMINAL_RESISTANCE 10000
-#define NOMINAL_TEMPERATURE 25
-#define BCOEFFICIENT 3950
-
-//configuração do sensor de umidade
-#define sensorPin  A1
-
-//definição de variaveis para temporização
-unsigned long lastReadingTime = 0; 
-const unsigned long interval = 1000;
-
-float TREAD;
-int RESTO = 0;
 int Ro = 100, B =  3950; 
 int Rseries = 8;
 float To = 298.15;
-unsigned long timer =0;
-unsigned long lastup=0;
+float TREAD;
 
+double temp = 0;
 
-#include <ThreeWire.h>
-#include <RtcDS1302.h>
+bool configMode = false;
+#include <ThreeWire.h> //INCLUSÃO DA BIBLIOTECA
+#include <RtcDS1302.h> //INCLUSÃO DA BIBLIOTECA
 
-class RTCManager {
-public:
-    RTCManager() : myWire(4, 5, 2), Rtc(myWire) {}
+#include <SD.h>
 
-    void initialize() {
-        Serial.begin(9600);
-        Rtc.Begin();
-        Serial.print("Compilado em: ");
-        RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__);
-        printDateTime(compiled);
-        Serial.println();
-        Serial.println();
+const int chipSelect = 4;  // CS pin for SD card module
 
-        if (Rtc.GetIsWriteProtected()) {
-            Serial.println("RTC está protegido contra gravação. Habilitando a gravação agora...");
-            Rtc.SetIsWriteProtected(false);
-            Serial.println();
-        }
+File dataFile;
 
-        if (!Rtc.GetIsRunning()) {
-            Serial.println("RTC não está funcionando de forma contínua. Iniciando agora...");
-            Rtc.SetIsRunning(true);
-            Serial.println();
-        }
+ThreeWire myWire(4, 5, 2); //OBJETO DO TIPO ThreeWire
+RtcDS1302<ThreeWire> Rtc(myWire); //OBJETO DO TIPO RtcDS1302
 
-        RtcDateTime now = Rtc.GetDateTime();
-        if (now < compiled) {
-            Serial.println("As informações atuais do RTC estão desatualizadas. Atualizando informações...");
-            Rtc.SetDateTime(compiled);
-            Serial.println();
-        }
-        else if (now > compiled) {
-            Serial.println("As informações atuais do RTC são mais recentes que as de compilação. Isso é o esperado.");
-            Serial.println();
-        }
-        else if (now == compiled) {
-            Serial.println("As informações atuais do RTC são iguais as de compilação! Não é o esperado, mas está tudo OK.");
-            Serial.println();
-        }
-    }
-
-    void loop() {
-        RtcDateTime now = Rtc.GetDateTime();
-        printDateTime(now);
-        Serial.println();
+void setup() {
+  // initialize serial communications at 9600 bps:
+  Serial.begin(9600);
+  while (!Serial) {
         delay(1000);
     }
+    Serial.begin(9600); //INICIALIZA A SERIAL
+    Rtc.Begin(); //INICIALIZA O RTC
+    Serial.print("Compilado em: "); //IMPRIME O TEXTO NO MONITOR SERIAL
+    RtcDateTime compiled = RtcDateTime(__DATE__, __TIME__); //VARIÁVEL RECEBE DATA E HORA DE COMPILAÇÃO
+    printDateTime(compiled); //PASSA OS PARÂMETROS PARA A FUNÇÃO printDateTime
+    Serial.println(); //QUEBRA DE LINHA NA SERIAL
+    Serial.println(); //QUEBRA DE LINHA NA SERIAL
 
-    void setDateTimeManually() {
-        Serial.println("Configurando data e hora manualmente...");
-
-        int year, month, day, hour, minute, second;
-
-        Serial.print("Ano (ex. 2023): ");
-        while (!Serial.available()) {}
-        year = Serial.parseInt();
-        Serial.println(year);
-
-        Serial.print("Mês (1-12): ");
-        while (!Serial.available()) {}
-        month = Serial.parseInt();
-        Serial.println(month);
-
-        Serial.print("Dia (1-31): ");
-        while (!Serial.available()) {}
-        day = Serial.parseInt();
-        Serial.println(day);
-
-        Serial.print("Hora (0-23): ");
-        while (!Serial.available()) {}
-        hour = Serial.parseInt();
-        Serial.println(hour);
-
-        Serial.print("Minuto (0-59): ");
-        while (!Serial.available()) {}
-        minute = Serial.parseInt();
-        Serial.println(minute);
-
-        Serial.print("Segundo (0-59): ");
-        while (!Serial.available()) {}
-        second = Serial.parseInt();
-        Serial.println(second);
-
-        RtcDateTime newDateTime(year, month, day, hour, minute, second);
-        Rtc.SetDateTime(newDateTime);
-
-        Serial.println("Configuração concluída!");
+    if(Rtc.GetIsWriteProtected()){ //SE O RTC ESTIVER PROTEGIDO CONTRA GRAVAÇÃO, FAZ
+        Serial.println("RTC está protegido contra gravação. Habilitando a gravação agora..."); //IMPRIME O TEXTO NO MONITOR SERIAL
+        Rtc.SetIsWriteProtected(false); //HABILITA GRAVAÇÃO NO RTC
+        Serial.println(); //QUEBRA DE LINHA NA SERIAL
     }
-    void getDate(){}
-    void getTime(){}
-    
-private:
-    ThreeWire myWire;
-    RtcDS1302<ThreeWire> Rtc;
 
-    void printDateTime(const RtcDateTime& dt) {
-        char datestring[20];
-        snprintf_P(datestring,
-                   countof(datestring),
-                   PSTR("%02u/%02u/%04u %02u:%02u:%02u"),
-                   dt.Day(),
-                   dt.Month(),
-                   dt.Year(),
-                   dt.Hour(),
-                   dt.Minute(),
-                   dt.Second());
-        Serial.print(datestring);
+    if(!Rtc.GetIsRunning()){ //SE RTC NÃO ESTIVER SENDO EXECUTADO, FAZ
+        Serial.println("RTC não está funcionando de forma contínua. Iniciando agora..."); //IMPRIME O TEXTO NO MONITOR SERIAL
+        Rtc.SetIsRunning(true); //INICIALIZA O RTC
+        Serial.println(); //QUEBRA DE LINHA NA SERIAL
     }
-};
 
-RTCManager rtcManager;
-
-
-
-void setup(){
-    Serial.begin(9600);
-    Serial.print("Initializing SD card...");
-
-
-    pinMode(CSpin, OUTPUT);
-    rtcManager.initialize();
-    if (!SD.begin(CSpin)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    return;
+    RtcDateTime now = Rtc.GetDateTime(); //VARIÁVEL RECEBE INFORMAÇÕES
+    if (now < compiled) { //SE A INFORMAÇÃO REGISTRADA FOR MENOR QUE A INFORMAÇÃO COMPILADA, FAZ
+        Serial.println("As informações atuais do RTC estão desatualizadas. Atualizando informações..."); //IMPRIME O TEXTO NO MONITOR SERIAL
+        Rtc.SetDateTime(compiled); //INFORMAÇÕES COMPILADAS SUBSTITUEM AS INFORMAÇÕES ANTERIORES
+        Serial.println(); //QUEBRA DE LINHA NA SERIAL
     }
-    Serial.println("card initialized.");
-      if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-      Serial.println(F("SSD1306 allocation failed"));
-      for(;;); 
+    else if (now > compiled){ //SENÃO, SE A INFORMAÇÃO REGISTRADA FOR MAIOR QUE A INFORMAÇÃO COMPILADA, FAZ
+        Serial.println("As informações atuais do RTC são mais recentes que as de compilação. Isso é o esperado."); //IMPRIME O TEXTO NO MONITOR SERIAL
+        Serial.println(); //QUEBRA DE LINHA NA SERIAL
     }
-    
-    display.clearDisplay();
-    display.setTextSize(1);             
-    display.setTextColor(SSD1306_WHITE);        
-    display.setCursor(0,0);             
-    display.print("INICIANDO...");
-    display.display();
-    delay(500);
+    else if (now == compiled) { //SENÃO, SE A INFORMAÇÃO REGISTRADA FOR IGUAL A INFORMAÇÃO COMPILADA, FAZ
+        Serial.println("As informações atuais do RTC são iguais as de compilação! Não é o esperado, mas está tudo OK."); //IMPRIME O TEXTO NO MONITOR SERIAL
+        Serial.println(); //QUEBRA DE LINHA NA SERIAL
+    }
+    // Print initial message
+    Serial.println("Type 'config' to enter configuration mode.");
+    if (!SD.begin(chipSelect)) {
+      Serial.println(F("Card failed, or not present"));
+      return;
+    }
+    Serial.println(F("Card initialized."));
+  
+    // Open a file named "data.csv"
+    dataFile = SD.open("data.csv", FILE_WRITE);
+  
+    if (dataFile) {
+      Serial.println(F("File opened successfully."));
+      dataFile.println("Temperature,Moisture");  // Write header to the file
+      //dataFile.close();
+    } else {
+      Serial.println(F("Error opening file."));
+    }
+    // Enter the loop
+    loop();
 }
 
+#define countof(a) (sizeof(a) / sizeof(a[0]))
 
-void loop(){
-    if (millis() - lastReadingTime >= interval) {
-        sensorReading1 += 0.00; // value read from your first sensor
-        sensorReading2 += 0.00; // value read from your second sensor
-        sensorReading3 += 0.00;
-        getTemp();
-        //getMoisture();
-        atualizadisplay();
-        lastReadingTime = millis();
+//ACESSORES ARQUIVOS E FORMATACAO
+
+void appendDataToFile(String data) {
+  // Open the file in append mode
+  dataFile = SD.open("data.csv", FILE_WRITE);
+
+  if (dataFile) {
+    dataFile.println(data);  // Append data to the file
+    //dataFile.close();
+    Serial.println(F("Data appended to file."));
+  } else {
+    Serial.println(F("Error appending data to file."));
   }
-
 }
 
-
+// AQUISICAO DE DADOS DOS SENSORES
 int getMoisture(){
-    int sensorValue = analogRead(sensorPin);
+    int sensorValue = analogRead(A1);
     int moisturePercentage = map(sensorValue, 0, 1023, 100, 0);
-    Serial.print("Raw Value: ");
-    Serial.print(sensorValue);
-    Serial.print("\t Moisture Percentage: ");
-    Serial.println(moisturePercentage);
     return moisturePercentage;
 }
 
-float getTemp(){
-    float Vi = analogRead(A2) * (5.0 / 1023.0);
-    float R = (Vi * Rseries) / (5- Vi);
-    float T =  1 / ((1 / To) + ((log(R / Ro)) / B));
-    TREAD= T - 273.15;
-    //if(TREAD>35 && TREAD<36){}   
-    
-    int temp = TREAD;
-    Serial.println(temp);
-    return temp;
+int getTemp(){
+  float Vi = analogRead(A0) * (5.0 / 1023.0);
+  float R = (Vi * Rseries) / (5- Vi);
+  float T =  1 / ((1 / To) + ((log(R / Ro)) / B));
+  TREAD= T - 273.15;
+  //Serial.print(TREAD);
+  return TREAD;
 }
 
-void atualizadisplay(){
-  if((millis()-lastup)>150){
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setCursor(0,0);           
-    //display.print(F("TEMP:"));
-    //display.println(TSET);
-    //display.setTextSize(1);            
-    display.setTextColor(SSD1306_WHITE);       
-    display.print(F("TEMPERATURA ATUAL:"));
-    display.setCursor(0,20);                 
-    //display.setCursor(0, 30);   
-    display.setTextSize(4);             
-    float TDISP = TREAD;
-    display.println(TDISP);
-    display.display();
-    lastup=millis();
-  } 
-}
-/*
-Adicionar ao codigo: menu de configuraçao do horario via terminal
+void printDateTime(const RtcDateTime& dt){
+    char datestring[20]; //VARIÁVEL ARMAZENA AS INFORMAÇÕES DE DATA E HORA
 
-*/
-void configTime(){
-    //adicionar logica de configuração da data e hora no modulo rtc
-}
-void getTime(){
-    //adicionar logica de aquisição de data e hora
+    snprintf_P(datestring, 
+            countof(datestring),
+            PSTR("%02u/%02u/%04u %02u:%02u:%02u"), //FORMATO DE EXIBIÇÃO DAS INFORMAÇÕES
+            dt.Day(), //DIA
+            dt.Month(), //MÊS
+            dt.Year(), //ANO
+            dt.Hour(), //HORA
+            dt.Minute(), //MINUTOS
+            dt.Second() ); //SEGUNDOS
+    Serial.print(datestring); //IMPRIME NO MONITOR SERIAL AS INFORMAÇÕES
 }
 
-void saveData(){
-    unsigned long currentMillis = millis();
 
-    // Calculate date and time components
-    unsigned long seconds = currentMillis / 1000;
-    unsigned long minutes = seconds / 60;
-    unsigned long hours = minutes / 60;
-    unsigned long days = hours / 24;
+//-------------------------------------------------------------
+//CONFIGURACAO DO MODULO RTC
 
-    // Extract individual components
-    int year = 2000; // Set a default year
-    int month, day, hour, minute;
+void setDateTimeManually() {
+    Serial.println("Enter the date and time in the format 'YYYY-MM-DD HH:MM:SS'");
+    Serial.println("Use a space between date and time components.");
+    Serial.println("Press Enter when done.");
 
-    // Calculate date and time components
-    minute = minutes % 60;
-    hour = hours % 24;
-    day = days % 31 + 1; // A simple placeholder for day
-    month = (days / 31) % 12 + 1; // A simple placeholder for month
+    while (!Serial.available()) {
+        // Wait for user input
+    }
 
-    // Build the formatted timestamp string
-    String formattedTimestamp = String(day) + '/' + String(month) + '/' + String(year) +
-                                ", " + String(hour) + ':' + String(minute);
+    // Read user input
+    String input = Serial.readStringUntil('\n');
 
-    // Build the data string
-    dataString = formattedTimestamp + ", " + String(getMoisture()) + ", " + String(getTemp());
+    // Parse the input
+    int year, month, day, hour, minute, second;
+    if (sscanf(input.c_str(), "%d-%d-%d %d:%d:%d", &year, &month, &day, &hour, &minute, &second) == 6) {
+        RtcDateTime newDateTime(year, month, day, hour, minute, second);
 
-    if(SD.exists("data.csv")){ // check if the card is still there
-        // now append the new data to the file
-        sensorData = SD.open("data.csv", FILE_WRITE);
-        if (sensorData){
-            sensorData.println(dataString);
-            sensorData.close(); // close the file
+        // Update the RTC
+        Rtc.SetDateTime(newDateTime);
+
+        Serial.println("Date and time set successfully!");
+        Serial.print("New date and time: ");
+        printDateTime(newDateTime);
+    } else {
+        Serial.println("Invalid input. Please try again.");
+        setDateTimeManually();  // Retry if the input is invalid
+    }
+}
+
+String dataFormat = "";
+
+//LOOP PRINCIPAL
+void loop() {
+   // Check for commands from Serial Monitor
+    if (Serial.available()) {
+        String command = Serial.readStringUntil('\n');
+        if (command == "config") {
+            Serial.println("Entering configuration mode. Type the new date and time.");
+            configMode = true;
         }
+        // Add more commands as needed
     }
-    else{
-        Serial.println("Error writing to file !");
+    if (configMode) {
+        // Configuration mode
+        setDateTimeManually();
+        configMode = false;  // Exit configuration mode after setting date and time
+    } else {
+        float temperatura = getTemp();
+        int umidade = getMoisture();
+//        
+//        sensorValue = analogRead(A0);
+//        outputValue = map(sensorValue, 0, 1023, 0, 255);
+//        //Serial.print(temperatura);
+//        //Serial.print(",");
+//
+//        sensorValue2 = analogRead(A1);
+//        outputValue2 = map(sensorValue2, 0, 1023, 0, 255);
+//        //Serial.println(umidade);
+
+        
+         dataFormat =  String(getMoisture()) + "," + String(getTemp());
+         Serial.println(dataFormat);
+
+
     }
+
+   
+
+    // Wait before the next loop
+    delay(50);
 }
